b37336f1e09664fd14c5784ed5cad32842604567
Marcin Kos <marcin.kos.krk@gmail.com>
2021-11-25 18:58:36 +0100

    admin and users

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # local
+    "user.apps.UserConfig",
+    # 3rd party
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'user.CustomUser'
