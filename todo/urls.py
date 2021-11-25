473d68c10455b835d1942d7d89f7aa0b4e363ee3
Marcin Kos <marcin.kos.krk@gmail.com>
2021-11-25 22:28:30 +0100

    filtering tasks

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,7 @@ from todo import views
 app_name = 'todo'
 urlpatterns = [
     path('', views.TaskView.as_view(), name='todo'),
+    path('<int:pk>', views.DeleteTaskView.as_view(), name='delete'),
+    path('done/<int:pk>', views.DoneTaskView.as_view(), name='done'),
+    path('filter/<str:filter>', views.TaskView.as_view(), name='filter'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,42 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.views import View
+from django.views.generic import CreateView, UpdateView
 
 from todo.models import Task
 
 
 class TaskView(CreateView):
     model = Task
-    fields = "__all__"  # " " - with them django doesn't check automatically if exist
+    fields = ['details', 'owner']  # " " - with them django doesn't check automatically if exist
     template_name = 'to_do/todo.html'
     success_url = reverse_lazy('todo:todo')
 
     def get_context_data(self, **kwargs):
+        filter_ = self.kwargs.get('filter', None)
         context = super().get_context_data(**kwargs)
-        context['task_list'] = Task.objects.filter(is_done=False, archive=False)
+        if filter_ is None or filter_ == 'ongoing':
+            context['task_list'] = Task.objects.filter(is_done=False, archive=False)
+        elif filter_ == 'all':
+            context['task_list'] = Task.objects.filter(archive=False)
+        elif filter_ == 'done':
+            context['task_list'] = Task.objects.filter(is_done=True, archive=False)
+
         return context
+
+
+class DeleteTaskView(View):
+    def get(self, request, pk):
+        task = Task.objects.get(pk=pk)
+        task.archive = True
+        task.save()
+        return HttpResponseRedirect(reverse('todo:todo'))
+
+
+class DoneTaskView(View):
+    def post(self, request, pk):
+        task = Task.objects.get(pk=pk)
+        task.is_done = True
+        task.save()
+        return HttpResponseRedirect(reverse('todo:todo'))
